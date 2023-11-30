c6f7034e67d86e76287596c5afe8689564d97ccb
dynamic-designer <w3.vimal@gmail.com>
2023-11-30 10:07:16 +0530

    new changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
 
     "use strict";
 
@@ -14,10 +14,49 @@
                 slidesToShow: 3,
                 dots: false,
                 arrows: true,
-                autoplay: false ,
+                autoplay: false,
                 autoplaySpeed: 4000,
                 prevArrow: '<div class="slick-prev"><span class="arrow-icon"><i data-feather="chevron-left"></i></span></div>',
                 nextArrow: '<div class="slick-next"><span class="arrow-icon"><i data-feather="chevron-right"></i></span></div>',
+            });
+        }
+    }
+
+    function updatesSlider() {
+        if ($(".updateVertical-slider").length) {
+            $(".updateVertical-slider").slick({
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                autoplay: false,
+                autoplaySpeed: 4000,
+                infinite: true,
+                vertical: true,
+                asNavFor: '.updateHorizontal-slider',
+                arrows: true,
+                prevArrow: '<div class="slick-prev"><span class="arrow-icon"><i data-feather="chevron-left"></i></span></div>',
+                nextArrow: '<div class="slick-next"><span class="arrow-icon"><i data-feather="chevron-right"></i></span></div>',
+            });
+        }
+        if ($(".updateHorizontal-slider").length) {
+            $(".updateHorizontal-slider").slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: false,
+                autoplaySpeed: 4000,
+                infinite: true,
+                asNavFor: '.updateVertical-slider',
+                arrows: true,
+                prevArrow: '<div class="slick-prev slick-btn"><span class="arrow-icon"><i data-feather="chevron-left"></i></span></div>',
+                nextArrow: '<div class="slick-next slick-btn"><span class="arrow-icon"><i data-feather="chevron-right"></i></span></div>',
+                responsive: [
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1,
+                            asNavFor: '',
+                        }
+                    }
+                ]
             });
         }
     }
@@ -29,10 +68,11 @@
     $(document).ready(function () {
 
         productSlider();
-        
+        updatesSlider();
+
         //Feather Icon Init
         feather.replace();
-        
+
     });
 
 
@@ -40,7 +80,7 @@
     /*==========================================================================
         WHEN WINDOW SCROLL
     ==========================================================================*/
-    $(window).on("scroll", function() {
+    $(window).on("scroll", function () {
         if ($(window).scrollTop() > 300) {
             backToTopBtn.addClass('show');
         } else {
@@ -48,11 +88,11 @@
         }
     });
 
-    
+
     /*==========================================================================
         WHEN WINDOW RESIZE
     ==========================================================================*/
-    $(window).on("resize", function() {
+    $(window).on("resize", function () {
 
     });
 
