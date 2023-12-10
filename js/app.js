9165c0a4d048ca49bd36212204622143de3366d3
dynamic-designer <w3.vimal@gmail.com>
2023-12-10 15:21:21 +0530

    index page all changes

--- js/app.js
+++ js/app.js
@@ -1,23 +1,76 @@
 (function ($) {
-
     "use strict";
 
-    var backToTopBtn = $('#backToTopButton');
+    var backToTopBtn = $("#backToTopButton");
 
     /*------------------------------------------
-        = FUNCTIONS
-    -------------------------------------------*/
+          = FUNCTIONS
+      -------------------------------------------*/
+
+    // Hero slider background setting
+    function sliderBgSetting() {
+        if ($(".hero-slider .slide").length) {
+            $(".hero-slider .slide").each(function () {
+                var $this = $(this);
+                var img = $this.find(".slider-bg").attr("src");
+
+                $this.css({
+                    backgroundImage: "url(" + img + ")",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center center"
+                })
+            });
+        }
+    }
+
+    //Setting hero slider
+    function heroSlider() {
+        if ($(".hero-slider").length) {
+            $(".hero-slider").slick({
+                autoplay: false,
+                autoplaySpeed: 6000,
+                pauseOnHover: true,
+                infinite: true,
+                arrows: false,
+                dots: true,
+                fade: true,
+                cssEase: 'linear'
+            });
+        }
+    }
+
+    // Parallax background
+    function bgParallax() {
+        if ($(".parallax").length) {
+            $(".parallax").each(function () {
+                var height = $(this).position().top;
+                var resize = height - $(window).scrollTop();
+                var doParallax = -(resize / 5);
+                var positionValue = doParallax + "px";
+                var img = $(this).data("bg-image");
+
+                $(this).css({
+                    backgroundImage: "url(" + img + ")",
+                    backgroundPosition: "50%" + positionValue,
+                    backgroundSize: "cover"
+                });
+            });
+        }
+    }
+
     function productSlider() {
-        if ($(".product-slider").length) {
-            $(".product-slider").slick({
+        if ($(".product-slider, .faq-slider").length) {
+            $(".product-slider, .faq-slider").slick({
                 infinite: true,
                 slidesToShow: 3,
                 dots: false,
                 arrows: true,
                 autoplay: false,
                 autoplaySpeed: 4000,
-                prevArrow: '<div class="slick-prev"><span class="arrow-icon"><i data-feather="chevron-left"></i></span></div>',
-                nextArrow: '<div class="slick-next"><span class="arrow-icon"><i data-feather="chevron-right"></i></span></div>',
+                prevArrow:
+                    '<div class="slick-prev"><span class="btn btn-primary"><i data-feather="arrow-left-circle"></i></span></div>',
+                nextArrow:
+                    '<div class="slick-next"><span class="btn btn-primary"><i data-feather="arrow-right-circle"></i></span></div>'
             });
         }
     }
@@ -31,10 +84,12 @@
                 autoplaySpeed: 4000,
                 infinite: true,
                 vertical: true,
-                asNavFor: '.updateHorizontal-slider',
+                asNavFor: ".updateHorizontal-slider",
                 arrows: true,
-                prevArrow: '<div class="slick-prev"><span class="arrow-icon"><i data-feather="chevron-left"></i></span></div>',
-                nextArrow: '<div class="slick-next"><span class="arrow-icon"><i data-feather="chevron-right"></i></span></div>',
+                prevArrow:
+                    '<div class="slick-prev"><span class="btn btn-primary"><i data-feather="arrow-left-circle"></i></span></div>',
+                nextArrow:
+                    '<div class="slick-next"><span class="btn btn-primary"><i data-feather="arrow-right-circle"></i></span></div>'
             });
         }
         if ($(".updateHorizontal-slider").length) {
@@ -44,16 +99,18 @@
                 autoplay: false,
                 autoplaySpeed: 4000,
                 infinite: true,
-                asNavFor: '.updateVertical-slider',
+                asNavFor: ".updateVertical-slider",
                 arrows: true,
-                prevArrow: '<div class="slick-prev slick-btn"><span class="arrow-icon"><i data-feather="chevron-left"></i></span></div>',
-                nextArrow: '<div class="slick-next slick-btn"><span class="arrow-icon"><i data-feather="chevron-right"></i></span></div>',
+                prevArrow:
+                    '<div class="slick-prev slick-btn"><span class="btn btn-primary"><i data-feather="arrow-left-circle"></i></span></div>',
+                nextArrow:
+                    '<div class="slick-next slick-btn"><span class="btn btn-primary"><i data-feather="arrow-right-circle"></i></span></div>',
                 responsive: [
                     {
                         breakpoint: 768,
                         settings: {
                             slidesToShow: 1,
-                            asNavFor: '',
+                            asNavFor: ""
                         }
                     }
                 ]
@@ -61,39 +118,38 @@
         }
     }
 
-
     /*==========================================================================
-        WHEN DOCUMENT LOADING 
-    ==========================================================================*/
+          WHEN DOCUMENT LOADING 
+      ==========================================================================*/
     $(document).ready(function () {
-
+        sliderBgSetting();
+        heroSlider();
         productSlider();
         updatesSlider();
 
+        var windowHeight = $(window).outerHeight()
+        var headerHeight = $('header').outerHeight();
+        $(".hero-slider-wrapper").css("height", (windowHeight - 200)+"px");
+
         //Feather Icon Init
         feather.replace();
-
     });
 
-
-
     /*==========================================================================
-        WHEN WINDOW SCROLL
-    ==========================================================================*/
+          WHEN WINDOW SCROLL
+      ==========================================================================*/
     $(window).on("scroll", function () {
+        bgParallax();
+
         if ($(window).scrollTop() > 300) {
-            backToTopBtn.addClass('show');
+            backToTopBtn.addClass("show");
         } else {
-            backToTopBtn.removeClass('show');
+            backToTopBtn.removeClass("show");
         }
     });
 
-
     /*==========================================================================
-        WHEN WINDOW RESIZE
-    ==========================================================================*/
-    $(window).on("resize", function () {
-
-    });
-
+          WHEN WINDOW RESIZE
+      ==========================================================================*/
+    $(window).on("resize", function () { });
 })(window.jQuery);
